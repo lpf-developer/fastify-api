c53e148780dc2edb4959387e03f7c310d829ba87
Luis Paulo <franca.luiz.paulo.777@hotmail.com>
2023-03-16 11:17:12 -0300

    Configuração Restify-api-mongoose

---
+++ src/user.js
@@ -0,0 +1,8 @@
+//User.js
+const mongoose = require('mongoose')
+let UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String
+})
+module.exports = mongoose.model('User', UserSchema)
